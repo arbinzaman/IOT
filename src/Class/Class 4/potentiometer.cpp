13ed68b807e61b78796b79d24820df123088cfb0
arbinzaman <arbinzmn@gmail.com>
2024-10-05 22:33:51 +0600

    Chore: Added Potentiometer codes from Class 4 task

---
+++ src/Class/Class 4/potentiometer.cpp
@@ -0,0 +1,23 @@
+// C++ code
+//
+
+#include <Arduino.h>
+const int led = 13;
+const int pot = 7;
+
+void setup()
+{ 
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+  pinMode(pot,INPUT);
+}
+
+void loop()
+{
+  int potvalue = analogRead(pot);
+  Serial.println(potvalue);
+  digitalWrite(led,HIGH);
+  delay(potvalue);
+  digitalWrite(led,LOW);
+  delay(potvalue);
+}
